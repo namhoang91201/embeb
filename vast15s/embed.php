a36f61991abfc9fe094092551ec0d6faa65c54c5
namhoang91201 <67616560+namhoang91201@users.noreply.github.com>
2020-07-10 18:24:38 -0700

    Update embed.php

--- vast15s/embed.php
+++ vast15s/embed.php
@@ -1,7 +1,6 @@
 <meta charset="UTF-8">
 <script src="../vast15s/embed/jquery.min.js" type="text/javascript"></script>
-<script type="text/javascript" src="http://tvhay.org/playergk/jwplayer885/jwplayer885k.js"></script>
-<script>jwplayer.key="MBvrieqNdmVL4jV0x6LPJ0wKB/Nbz2Qq/lqm3g=="</script>
+ <script src='../vast15s/aVr2lJgW.js'></script>
 <div id="adsmessage" class="adsmessage" style="display:none;"></div>
 <div id="playerjw7"></div>
 
